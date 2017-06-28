65e6b1ff732bae2116015fbabb48761a97081c6f
Vincent Bisserie <vbisserie@gmail.com>
2017-06-28 11:44:44 -0400

    SEC-52: Improve test for regex_replace

--- osquery/sql/tests/sql_tests.cpp
+++ osquery/sql/tests/sql_tests.cpp
@@ -157,43 +157,48 @@ TEST_F(SQLTests, test_sql_sha256) {
 
 TEST_F(SQLTests, test_sql_regex_split) {
   QueryData d;
-  query("select regex_split('10-0.12.192', '[-\\.]', 1) as test;", d);
+  auto result = query("select regex_split('10-0.12.192', '[-\\.]', 1) as test;", d);
+  EXPECT_TRUE(result.ok());
   EXPECT_EQ(d.size(), 1U);
   EXPECT_EQ(d[0]["test"], "0");
 }
 
 TEST_F(SQLTests, test_sql_regex_replace) {
   QueryData d;
-  query(
+  auto result = query(
       "select regex_replace('5 little monkeys climbing up 1 wall', '[0-9]', "
       "'-') as test;",
       d);
+  EXPECT_TRUE(result.ok());
   EXPECT_EQ(d.size(), 1U);
   EXPECT_EQ(d[0]["test"], "- little monkeys climbing up - wall");
 }
 
 TEST_F(SQLTests, test_sql_regex_replace_with_empty_pattern) {
   QueryData d;
-  query(
+  auto result = query(
       "select regex_replace('5 little monkeys climbing up 1 wall', '', '-') as "
       "test;",
       d);
+  EXPECT_FALSE(result.ok());
   EXPECT_EQ(d.size(), 0);
 }
 
 TEST_F(SQLTests, test_sql_regex_replace_with_empty_input) {
   QueryData d;
-  query("select regex_replace('', '[0-9]', '-') as test;", d);
+  auto result = query("select regex_replace('', '[0-9]', '-') as test;", d);
+  EXPECT_TRUE(result.ok());
   EXPECT_EQ(d.size(), 1U);
   EXPECT_EQ(d[0]["test"], "");
 }
 
 TEST_F(SQLTests, test_sql_regex_replace_without_match) {
   QueryData d;
-  query(
+  auto result = query(
       "select regex_replace('Mama said the monkeys,not to fight',"
       " '[0-9]', '-') as test;",
       d);
+  EXPECT_TRUE(result.ok());
   EXPECT_EQ(d.size(), 1U);
   EXPECT_EQ(d[0]["test"], "Mama said the monkeys,not to fight");
 }
